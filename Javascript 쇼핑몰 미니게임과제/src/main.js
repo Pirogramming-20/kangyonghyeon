4411f3639e620f99603c0df3e2ada75b3db87b98
kangyonghyeon <khyeon001003@gmail.com>
2024-01-03 01:42:38 +0900

    1-7강 Javascript-handling events

--- Javascript 쇼핑몰 미니게임과제/src/main.js
+++ Javascript 쇼핑몰 미니게임과제/src/main.js
@@ -8,11 +8,14 @@ function loadItems(){
 
 //loadItems에 json.items가 리턴이 되고 밑에 함수애서 item이라는 변수에 loaditems값이 들어간다
 
+//update the list with given items
 function displayItems(items){
   const container = document.querySelector('.items');
   container.innerHTML =items.map(item => createHTMLString(item)).join('');
+  //문자열로 바꿔서 join으로 li들 엮어서 출력-> html에 li태그들이 추가된다
 }
 
+//create HTML list item from thr given data item
 function createHTMLString(item){
   return `
   <li class="item">
@@ -20,6 +23,27 @@ function createHTMLString(item){
     <span class="item__description">${item.gender} ,${item.size}</span>
   </li>
   `;
+}
+
+function setEventListeners(items){
+  const logo =document.querySelector('.logo');
+  const buttons =document.querySelector('.buttons');
+  logo.addEventListener('click',()=>displayItems(items));
+  buttons.addEventListener('click',event=> onButtonClick(event,items));
+
+}
+
+function onButtonClick(event,item){
+  const dataset=event.target.dataset;
+  const key =dataset.key;
+  const value =dataset.value;
+  if (key==null || value==null){
+    return;
+  }
+  const filtered =item.filter(item=>item[key]===value);
+  console.log(filtered)
+  displayItems(filtered);
+
 }
 //main
 
