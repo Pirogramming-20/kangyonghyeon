a88e3399f1cba9fefa86844c20c85d0aeabc3bd5
kangyonghyeon <khyeon001003@gmail.com>
2024-01-03 01:24:02 +0900

    1-6강 JavaScrip-displayitems

--- Javascript 쇼핑몰 미니게임과제/src/main.js
+++ Javascript 쇼핑몰 미니게임과제/src/main.js
@@ -6,14 +6,27 @@ function loadItems(){
 
 }
 
+//loadItems에 json.items가 리턴이 되고 밑에 함수애서 item이라는 변수에 loaditems값이 들어간다
 
+function displayItems(items){
+  const container = document.querySelector('.items');
+  container.innerHTML =items.map(item => createHTMLString(item)).join('');
+}
 
+function createHTMLString(item){
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+    <span class="item__description">${item.gender} ,${item.size}</span>
+  </li>
+  `;
+}
 //main
 
 loadItems()
 .then(items=>{
   console.log(items);
-/*   displayItems(items);
-  setEventListeners(items) */
+  displayItems(items);
+  setEventListeners(items) 
 })
 .catch(console.log)
