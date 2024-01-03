9df4577d166916f5f64fbe6f06577b9d56454b69
kangyonghyeon <khyeon001003@gmail.com>
2024-01-04 00:52:30 +0900

    숫자 야구 게임 진짜 완성

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -4,22 +4,27 @@ const submitbutton = document.querySelector('.submit-button');
 function try_go() {
   const sub = document.querySelectorAll('.input-field');
   const submit = Array.from(sub).map(input => parseInt(input.value, 10));
+  //int 같은 함수-> (원하는 값,10(진수))
   const Allinput =submit.every(value=> !isNaN(value));
+  //Allinput에 bool값이 들어간다 그때 every모든게 숫자면 true 그렇지 않으면 false
 
   if (!Allinput){
     console.log('다채워');
     clearinput();
     return;
-    
+    //Allinput 이 true면 실행하지않고 false실행한다
   }
   console.log(`Life: ${life}`);
   if (life === 0) {
     submitbutton.disabled = true;
+    //life=0이면 submition 버튼을 작동시키지않는다
   }
   clearinput();
+  //값이 들어올떄 마다 입력 필드를 지운다
 }
 
 submitbutton.addEventListener('click', try_go);
+//버튼 눌리면 trygo를 실해한다 subbbmit 버튼을 만들었고
 
 // 정답 생성 함수
 function answercreate(count, min, max) {
@@ -35,10 +40,11 @@ function answercreate(count, min, max) {
   }
   return answ_num;
 }
-
+//python처럼 랜덤으로 숫자를 생성한다
 const answer = answercreate(3, 0, 9);
+//함수를 변수에 대입한다-> 그래서 answer를 만든다
 
-
+//check-numbers는 HTML에서 정의한 버튼이다
 // input을 array로 가져오기
 function check_numbers() {
   const sub = document.querySelectorAll('.input-field');
@@ -50,14 +56,11 @@ function check_numbers() {
     console.log('다채워')
     return;
   }
+//똑같이 submit을 정의한다 근데 굳이 submit을 두번 정의할필요가 있을까..?
   handleGame(submit);
   console.log('Submit:', submit);
   console.log('Answer:', answer);
 }  
-  //에러 확인용
-/*   console.log('Submit:', submit);
-  console.log('Answer:', answer);
-  // */
   // 비교하기
 function handleGame(submit) {
   let strike = 0;
@@ -70,7 +73,7 @@ function handleGame(submit) {
       ball++;
     }
   }
-
+//자리가 같으면 스트라이크 include하면 ball준다. 
   if (strike === 3) {
     const success = document.getElementById('game-result-img');
     success.src = './success.png';
@@ -91,7 +94,7 @@ function handleGame(submit) {
     console.log(resultHTML);
     displayItems(resultHTML);
   }
-
+//이거는
   life--;
   console.log(`${life}`);
   if (life === 0) {
