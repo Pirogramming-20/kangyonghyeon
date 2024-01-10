080ae5e46d57f57e9d00ede6e0ce2cf769e145a8
kangyonghyeon <khyeon001003@gmail.com>
2024-01-10 13:40:42 +0900

    애플리케이션 확장하기

--- djanggirls/blog/urls.py
+++ djanggirls/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     #mysite에서 연결된 urls을 post라는 뷰로 연결한다
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #pk키의 인덱스로 링크를 만든다 <>부분에서 장고는 정수값을 기대하고 pk라는 변수를 뷰로 전송한다
 ]
