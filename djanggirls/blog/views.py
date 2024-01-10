a007c91a184e49cf8f1ace4850fbcb5e685d1bb4
kangyonghyeon <khyeon001003@gmail.com>
2024-01-10 12:22:46 +0900

    템플릿 동적 데이터

--- djanggirls/blog/views.py
+++ djanggirls/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-  return render(request,'blog/post_list.html',{})
+  posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  return render(request,'blog/post_list.html',{'posts':posts}) #앞부분이 html:뒷부분이 파이썬
