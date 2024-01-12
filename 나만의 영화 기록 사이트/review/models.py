21b3986edd14f6cbfd13da01affd800282eed2f7
khyeon1003 <154689763+khyeon1003@users.noreply.github.com>
2024-01-12 23:30:50 +0900

    model 주석 추가

--- 나만의 영화 기록 사이트/review/models.py
+++ 나만의 영화 기록 사이트/review/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Movie(models.Model):
   title=models.CharField(max_length=32)
   year=models.CharField(max_length=32)
-  #year을 숫자로 받는게 나으려나?
   genre=models.CharField(max_length=32)
   star=models.FloatField()
   poster = models.ImageField(null=True)
@@ -12,7 +11,8 @@ class Movie(models.Model):
   review=models.TextField()
   director=models.CharField(max_length=32)
   actor=models.CharField(max_length=32) 
-  #pillow패키지 설치 알리기.
+  
+#pillow패키지 설치했습니다!
 
 
 
